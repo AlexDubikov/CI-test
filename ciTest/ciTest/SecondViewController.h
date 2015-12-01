bfa0b7f40c4bc0161b62fe4243d543e8c81249d9
Alexander Dubikov <adb@brightbox.ru>
2015-12-01 12:07:42 +0300

    CI

---
+++ ciTest/ciTest/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  ciTest
+//
+//  Created by Alexander Dubikov on 01/12/15.
+//  Copyright © 2015 Alexander Dubikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ ciTest/ciTest/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  ciTest
+//
+//  Created by Alexander Dubikov on 01/12/15.
+//  Copyright © 2015 Alexander Dubikov. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
